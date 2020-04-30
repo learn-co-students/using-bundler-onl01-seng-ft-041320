6e3d4540961ce742fb9f8932faa80f96bf7bb275
dannypadron09 <danny.padron09@gmail.com>
2020-04-30 12:48:49 -0400

    Done.

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,10 @@
+# This is where the action is. This is where our app logic goes
+# We have to use all the work done in the environment file
+require_relative "../config/environment"
+
 Hashie
 Sinatra
 Octokit
 Pry
 AwesomePrint
+
